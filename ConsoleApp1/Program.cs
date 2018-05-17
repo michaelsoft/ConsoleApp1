455a02b281c05e710659d6ec8db673545123fd56
michaelsoft <michaellyk@qq.com>
2018-05-17 15:40:13 +0800

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static string numberRangeFormat = @"<\d+,\d+>";
+
+        static void Main(string[] args)
+        {
+            var strs = new YieldTest().GetStrings2();
+            foreach (var str in strs)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/YieldTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class YieldTest
+    {
+        public IEnumerable<string> GetStrings1()
+        {
+            return new string[]{ "a","b","c"};
+        }
+
+        public IEnumerable<string> GetStrings2()
+        {
+            yield return "a";
+        }
+    }
+}
